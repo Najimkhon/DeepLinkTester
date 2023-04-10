94ca0466b6f170057ec0b7c12ed49b2f95b8fc99
Najimkhon <qwerty1995.290@gmail.com>
2023-04-10 17:55:15 +0500

    Project setup

---
+++ app/src/main/java/com/example/deeplinktester/ui/fragments/HomeFragment.kt
@@ -0,0 +1,8 @@
+package com.example.deeplinktester.ui.fragments
+
+import com.example.deeplinktester.databinding.FragmentHomeBinding
+import com.example.deeplinktester.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+}
