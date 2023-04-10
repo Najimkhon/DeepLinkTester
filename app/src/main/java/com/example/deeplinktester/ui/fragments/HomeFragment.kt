029ad9f21a7076ca7f9e704bf82aaab7dcfb45ea
Najimkhon <qwerty1995.290@gmail.com>
2023-04-10 20:30:57 +0500

    Intent tester logic is added

--- app/src/main/java/com/example/deeplinktester/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/deeplinktester/ui/fragments/HomeFragment.kt
@@ -1,8 +1,28 @@
 package com.example.deeplinktester.ui.fragments
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
 import com.example.deeplinktester.databinding.FragmentHomeBinding
 import com.example.deeplinktester.ui.base.BaseFragment
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
 
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+    override fun setListeners() {
+        binding.btnCheck.setOnClickListener{
+            if (binding.etInput.text.toString().isNotEmpty()){
+                try {
+                    var myAction = Uri.parse(binding.etInput.text.toString())
+                    val intent = Intent(Intent.ACTION_VIEW, myAction)
+                    startActivity(intent)
+                }catch (e: ActivityNotFoundException){
+                    Toast.makeText(requireContext(), e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }else{
+                Toast.makeText(requireContext(), "Please, enter your link", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
